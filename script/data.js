88e66a9273c65e27f436426df94ee58bd54962e1
SalvatoreFerrara <140163275+Piu33@users.noreply.github.com>
2023-10-08 12:37:24 +0200

    add 06.10

--- script/data.js
+++ script/data.js
@@ -81,7 +81,8 @@ window.onload = function () {
 			{ x: new Date(2023, 8, 27), y:  23519 },
 			{ x: new Date(2023, 8, 28), y:  27643},
 			{ x: new Date(2023, 9, 2), y:  20782},
-			{ x: new Date(2023, 9, 3), y:  26024}
+			{ x: new Date(2023, 9, 3), y:  26024},
+			{ x: new Date(2023, 9, 6), y:  16996}
 
 
 
@@ -119,7 +120,8 @@ window.onload = function () {
 			{ x: new Date(2023, 8, 27), y:   4306.00  },
 			{ x: new Date(2023, 8, 28), y:   4347.50  },
 			{ x: new Date(2023, 9, 2), y:  4317.25},
-			{ x: new Date(2023, 9, 3), y:  4266.25}
+			{ x: new Date(2023, 9, 3), y:  4266.25},
+			{ x: new Date(2023, 9, 6), y:  4323.75}
     ]
 	},
 	{
@@ -149,7 +151,8 @@ window.onload = function () {
 			{ x: new Date(2023, 8, 27), y:   19.03  },
 			{ x: new Date(2023, 8, 28), y:   17.43 },
 			{ x: new Date(2023, 9, 2), y:  17.73},
-			{ x: new Date(2023, 9, 3), y:  19.23}
+			{ x: new Date(2023, 9, 3), y:  19.23},
+			{ x: new Date(2023, 9, 6), y:  17.78}
 
 
 		]
