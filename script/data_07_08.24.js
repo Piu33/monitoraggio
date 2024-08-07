3dc7169d2742b07e419638211a8870d2389d95d2
SalvatoreFerrara <totoferro21@gmail.com>
2024-08-07 12:23:20 +0200

    add 2 graph

--- script/data_07_08.24.js
+++ script/data_07_08.24.js
@@ -4,7 +4,7 @@
 window.onload = function () {
 	var chart = new CanvasJS.Chart("chartContainer", {
 	title:{
-		text: "Monitoraggio PIU33 vs S&P500",
+		text: "Monitoraggio PIU33 vs S&P500 and VIX",
     margin: 50,
     fontSize:50,
     fontFamily: "tahoma",
@@ -40,6 +40,19 @@ window.onload = function () {
 		includeZero: false,
 		suffix: ""
 	}],
+	axisY2: {
+		title: "VIX",
+		lineColor: "#f50505",
+		tickColor: "#f50505",
+		labelFontColor: "#f50505",
+		titleFontColor: "#f50505",
+    titleFontSize:30,
+    labelFontSize:30,
+    interval:2,
+		includeZero: false,
+		prefix: "",
+		suffix: ""
+	},
 	toolTip: {
 		shared: true
 	},
@@ -162,6 +175,21 @@ window.onload = function () {
 
 
 
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+			
 
     ]
 	},
@@ -281,324 +309,11 @@ window.onload = function () {
 
     ]
 	},
-
-]
-});
-var chart1 = new CanvasJS.Chart("chartContainer1", {
-	title:{
-			text: "Performance (%) PIU33 vs S&P500 e correlazione VIX",
-			margin: 50,
-			fontSize: 50,
-			fontFamily: "tahoma",
-			color: "black"
-	},
-	axisX:{
-			interval: 1,
-			intervalType: "month",
-			labelFontSize: 20
-	},
-	axisY:[{
-			title: "PIU33",
-			lineColor: "#64a102",
-			tickColor: "#64a102",
-			labelFontColor: "#64a102",
-			titleFontColor: "#64a102",
-			titleFontSize: 30,
-			labelFontSize: 30,
-			interval: 5,
-			includeZero: true,
-			suffix: "",
-			minimum: -10, // Imposta il valore minimo
-			maximum: 50 // Imposta il valore massimo
-	},
-	{
-			title: "S&P500",
-			lineColor: "#141414",
-			tickColor: "#141414",
-			labelFontColor: "#141414",
-			titleFontColor: "#141414",
-			titleFontSize: 30,
-			labelFontSize: 30,
-			interval: 5,
-			includeZero: true,
-			suffix: "",
-			minimum: -10, // Imposta il valore minimo
-			maximum: 50 // Imposta il valore massimo
-	}],
-	axisY2: {
-			title: "VIX",
-			lineColor: "#f50505",
-			tickColor: "#f50505",
-			labelFontColor: "#f50505",
-			titleFontColor: "#f50505",
-			titleFontSize: 30,
-			labelFontSize: 30,
-			interval: 2,
-			includeZero: true,
-			prefix: "",
-			suffix: ""
-	},
-	toolTip: {
-			shared: true
-	},
-	legend: {
-			cursor: "pointer",
-			itemclick: toggleDataSeries
-	},
-	data: [{
-		type: "line",
-		name: "PIU33",
-		color: "#64a102",
-		showInLegend: true,
-		axisYIndex: 0,
-    lineThickness: 5,
-		dataPoints: [
-			{ x: new Date(2023, 6, 31), y: 0 },
-			{ x: new Date(2023, 7, 2), y: 0.52838},
-			{ x: new Date(2023, 7, 8), y: 0.95853 },
-			{ x: new Date(2023, 7, 16), y: 0.85787},
-			{ x: new Date(2023, 7, 24), y:0.77890 },
-			{ x: new Date(2023, 7, 28), y: 0.57890},
-			{ x: new Date(2023, 7, 29), y: 1.27648},
-			{ x: new Date(2023, 7, 30), y: 2.16134 },
-			{ x: new Date(2023, 8, 7), y: 1.56628},
-			{ x: new Date(2023, 8, 14), y: -0.81132 },
-			{ x: new Date(2023, 8, 15), y:-0.08627},
-			{ x: new Date(2023, 8, 19), y: 0.62403 },
-			{ x: new Date(2023, 8, 21), y: 1.11065 },
-			{ x: new Date(2023, 8, 22), y: 0.99957 },
-			{ x: new Date(2023, 8, 26), y: 1.28781 },
-			{ x: new Date(2023, 8, 27), y: 2.35192},
-			{ x: new Date(2023, 8, 28), y: 2.76433},
-			{ x: new Date(2023, 9, 2), y: 2.07823},
-			{ x: new Date(2023, 9, 3), y: 2.60235},
-			{ x: new Date(2023, 9, 6), y: 2.20457},
-			{ x: new Date(2023, 9, 10), y: 2.48112},
-			{ x: new Date(2023, 9, 13), y: 2.50870},
-			{ x: new Date(2023, 9, 17), y: 2.11047 },
-			{ x: new Date(2023, 9, 18), y: 2.68083 },
-			{ x: new Date(2023, 9, 20), y: 3.39141},
-			{ x: new Date(2023, 9, 26), y: 4.22935},
-			{ x: new Date(2023, 10, 1), y: 3.35775},
-			{ x: new Date(2023, 10, 2), y: 3.57857},
-			{ x: new Date(2023, 10, 3), y: 4.46821},
-			{ x: new Date(2023, 10, 7), y: 3.98336 },
-			{ x: new Date(2023, 10, 13), y: 3.58682 },
-			{ x: new Date(2023, 10, 14), y: 5.19160},
-			{ x: new Date(2023, 10, 16), y: 5.27550},
-			{ x: new Date(2023, 10, 22), y: 5.07445},
-			{ x: new Date(2023, 10, 30), y: 4.51545},
-			{ x: new Date(2023, 11, 11), y: 4.21075},
-			{ x: new Date(2023, 11, 14), y: 5.46985},
-			{ x: new Date(2023, 11, 19), y: 6.22478},
-			{ x: new Date(2023, 11, 21), y: 7.04508},
-			{ x: new Date(2023, 11, 22), y: 8.26913},
-			{ x: new Date(2024, 0, 3), y:  8.61818},
-			{ x: new Date(2024, 0, 10), y:  7.72688 },
-			{ x: new Date(2024, 0, 17), y:  7.68312},
-			{ x: new Date(2024, 0, 18), y: 7.92136},
-			{ x: new Date(2024, 0, 19), y: 8.03938},
-			{ x: new Date(2024, 0, 22), y: 8.25012},
-			{ x: new Date(2024, 0, 23), y: 7.54791},
-			{ x: new Date(2024, 0, 24), y: 8.92071},
-			{ x: new Date(2024, 0, 30), y: 8.71961},
-			{ x: new Date(2024, 0, 31), y: 9.42410},
-			{ x: new Date(2024, 1, 2), y:  10.02940 },
-			{ x: new Date(2024, 1, 8), y:  9.02720 },
-			{ x: new Date(2024, 1, 12), y: 10.18110},
-			{ x: new Date(2024, 1, 13), y: 11.26640},
-			{ x: new Date(2024, 1, 15), y: 11.85486},
-			{ x: new Date(2024, 1, 20), y: 11.47104},
-			{ x: new Date(2024, 1, 22), y: 13.45524},
-			{ x: new Date(2024, 1, 23), y: 14.12414},
-			{ x: new Date(2024, 2, 4), y: 13.66782},
-			{ x: new Date(2024, 2, 5), y:  14.42456},
-			{ x: new Date(2024, 2, 6), y: 13.04824},
-			{ x: new Date(2024, 2, 8), y: 14.84376},
-			{ x: new Date(2024, 2, 11), y: 16.07134},
-			{ x: new Date(2024, 2, 12), y: 15.88108},
-			{ x: new Date(2024, 2, 15), y: 15.45954},
-			{ x: new Date(2024, 2, 19), y: 15.35778},
-			{ x: new Date(2024, 2, 21), y: 16.63492},
-			{ x: new Date(2024, 3, 2), y:  15.17544},
-			{ x: new Date(2024, 3, 4), y: 15.50302},
-			{ x: new Date(2024, 3, 5), y: 17.28832},
-			{ x: new Date(2024, 3, 11), y: 17.31428},
-			{ x: new Date(2024, 3, 12), y: 17.92374},
-			{ x: new Date(2024, 3, 16), y: 19.26088},
-			{ x: new Date(2024, 3, 22), y: 16.96846},
-			{ x: new Date(2024, 3, 23), y: 17.52810},
-			{ x: new Date(2024, 3, 25), y: 18.10568},
-			{ x: new Date(2024, 3, 26), y:  19.12988},
-			{ x: new Date(2024, 4, 1), y:  18.28724},
-			{ x: new Date(2024, 4, 3), y:  17.30960},
-			{ x: new Date(2024, 4, 6), y:  17.30762},
-			{ x: new Date(2024, 4, 7), y:  18.21630},
-			{ x: new Date(2024, 4, 10), y: 17.41248},
-			{ x: new Date(2024, 4, 15), y:  16.06572},
-			{ x: new Date(2024, 4, 16), y:  16.18587 },
-			{ x: new Date(2024, 4, 31), y:  14.77801},
-			{ x: new Date(2024, 5, 3), y:   14.55956},
-			{ x: new Date(2024, 5, 5), y: 14.65927},
-			{ x: new Date(2024, 5, 6), y:  14.75839},
-			{ x: new Date(2024, 5, 12), y: 15.36751},
-			{ x: new Date(2024, 5, 20), y: 15.30405},
-			{ x: new Date(2024, 5, 28), y: 12.15178},
-			{ x: new Date(2024, 6, 1), y:  11.58105},
-			{ x: new Date(2024, 6, 3), y:  12.15113 },
-			{ x: new Date(2024, 6, 8), y: 12.73665},
-			{ x: new Date(2024, 6, 11), y: 13.44967},
-			{ x: new Date(2024, 6, 18), y:  16.14997},
-			{ x: new Date(2024, 6, 24), y: 16.74992},
-			{ x: new Date(2024, 6, 30), y: 17.55662},
-			{ x: new Date(2024, 6, 31), y: 20.22502},
-			{ x: new Date(2024, 7, 1), y:  22.57973},
-			{ x: new Date(2024, 7, 2), y: 29.96107 },
-			{ x: new Date(2024, 7, 5), y: 38.74411 },
-			{ x: new Date(2024, 7, 6), y: 42.41969}
-
-
-
-
-			
-
-
-
-
-
-
-
-
-
-
-
-
-
-			
-
-    ]
-	},
-	{
-		type: "line",
-		name: "S&P500",
-		color: "#141414",
-		axisYIndex:1,
-		showInLegend: true,
-    lineThickness: 5,
-		dataPoints: [
-			{ x: new Date(2023, 6, 31), y: 0.0000 },
-			{ x: new Date(2023, 7, 2), y: 0.0000 },
-			{ x: new Date(2023, 7, 8), y: -1.2456  },
-			{ x: new Date(2023, 7, 16), y: -2.0286 },
-			{ x: new Date(2023, 7, 24), y: -3.0273 },
-			{ x: new Date(2023, 7, 28), y: -2.5295},
-			{ x: new Date(2023, 7, 29), y: -1.5078 },
-			{ x: new Date(2023, 7, 30), y: -0.4522  },
-			{ x: new Date(2023, 8, 7), y: -2.1016 },
-			{ x: new Date(2023, 8, 14), y: 0.0935  },
-			{ x: new Date(2023, 8, 15), y: -0.7621   },
-			{ x: new Date(2023, 8, 19), y: -1.6419 },
-			{ x: new Date(2023, 8, 21), y: -3.2090 },
-			{ x: new Date(2023, 8, 22), y: -3.4251 },
-			{ x: new Date(2023, 8, 26), y: -4.5070 },
-			{ x: new Date(2023, 8, 27), y: -5.3798 },
-			{ x: new Date(2023, 8, 28), y: -4.4207  },
-			{ x: new Date(2023, 9, 2), y: -5.1189},
-			{ x: new Date(2023, 9, 3), y: -6.3072},
-			{ x: new Date(2023, 9, 6), y: -4.9685},
-			{ x: new Date(2023, 9, 10), y: -3.2317 },
-			{ x: new Date(2023, 9, 13), y: -3.9274},
-			{ x: new Date(2023, 9, 17), y: -2.8404 },
-			{ x: new Date(2023, 9, 18), y: -3.8610},
-			{ x: new Date(2023, 9, 20), y: -6.3483 },
-			{ x: new Date(2023, 9, 26), y: -8.4064 },
-			{ x: new Date(2023, 10, 1), y: -7.3262},
-			{ x: new Date(2023, 10, 2), y: -5.1479},
-			{ x: new Date(2023, 10, 3), y: -3.6588},
-			{ x: new Date(2023, 10, 7), y: -3.3113 },
-			{ x: new Date(2023, 10, 13), y: -2.7272 },
-			{ x: new Date(2023, 10, 14), y: -0.5683},
-			{ x: new Date(2023, 10, 16), y: -0.8103},
-			{ x: new Date(2023, 10, 22), y: 0.6034 },
-			{ x: new Date(2023, 10, 30), y: 0.5706},
-			{ x: new Date(2023, 11, 11), y: 1.4257},
-			{ x: new Date(2023, 11, 14), y: 5.0162 },
-			{ x: new Date(2023, 11, 19), y: 5.7617 },
-			{ x: new Date(2023, 11, 21), y:  4.7280 },
-			{ x: new Date(2023, 11, 22), y:  5.6629 },
-			{ x: new Date(2024, 0, 3), y:   4.5599 },
-			{ x: new Date(2024, 0, 10), y:   5.4308 },
-			{ x: new Date(2024, 0, 17), y:   4.9010 },
-			{ x: new Date(2024, 0, 18), y:  5.3245   },
-			{ x: new Date(2024, 0, 19), y:  6.1246 },
-			{ x: new Date(2024, 0, 22), y: 7.1440},
-			{ x: new Date(2024, 0, 23), y:  7.0569 },
-			{ x: new Date(2024, 0, 24), y:  8.0669 },
-			{ x: new Date(2024, 0, 30), y:  8.5024 },
-			{ x: new Date(2024, 0, 31), y:  7.6855 },
-			{ x: new Date(2024, 1, 2), y:   8.7094  },
-			{ x: new Date(2024, 1, 8), y:    9.8277 },
-			{ x: new Date(2024, 1, 12), y:   10.5977  },
-			{ x: new Date(2024, 1, 13), y:   9.1773 },
-			{ x: new Date(2024, 1, 15), y:   10.0469 },
-			{ x: new Date(2024, 1, 20), y:   9.1170  },
-			{ x: new Date(2024, 1, 22), y:   10.9582},
-			{ x: new Date(2024, 1, 23), y:  12.8368 },
-			{ x: new Date(2024, 2, 4), y:  13.5024 },
-			{ x: new Date(2024, 2, 5), y:   12.6159 },
-			{ x: new Date(2024, 2, 6), y:  13.2926 },
-			{ x: new Date(2024, 2, 8), y:  14.4516 },
-			{ x: new Date(2024, 2, 11), y:  13.0746 },
-			{ x: new Date(2024, 2, 12), y:  14.0412 },
-			{ x: new Date(2024, 2, 15), y:  13.1190 },
-			{ x: new Date(2024, 2, 19), y:  13.9264 },
-			{ x: new Date(2024, 2, 21), y:  15.7384 },
-			{ x: new Date(2024, 3, 2), y:   14.2514  },
-			{ x: new Date(2024, 3, 4), y:   15.3805 },
-			{ x: new Date(2024, 3, 5), y:   14.3849 },
-			{ x: new Date(2024, 3, 11), y:  13.3600 },
-			{ x: new Date(2024, 3, 12), y:  13.2926 },
-			{ x: new Date(2024, 3, 16), y:  11.3274 },
-			{ x: new Date(2024, 3, 22), y:  9.8027 },
-			{ x: new Date(2024, 3, 23), y:  11.6315 },
-			{ x: new Date(2024, 3, 25), y:  10.3102},
-			{ x: new Date(2024, 3, 26), y:  12.2467},
-			{ x: new Date(2024, 4, 1), y:  10.7065},
-			{ x: new Date(2024, 4, 3), y:   12.2126 },
-			{ x: new Date(2024, 4, 6), y:   13.1818 },
-			{ x: new Date(2024, 4, 7), y:  13.9121},
-			{ x: new Date(2024, 4, 10), y:  14.5134 },
-			{ x: new Date(2024, 4, 15), y:  15.5172 },
-			{ x: new Date(2024, 4, 16), y:  16.2821  },
-			{ x: new Date(2024, 4, 31), y:  14.0795 },
-			{ x: new Date(2024, 5, 3), y:   15.0355  },
-			{ x: new Date(2024, 5, 5), y:   15.9450 },
-			{ x: new Date(2024, 5, 6), y:  16.5809 },
-			{ x: new Date(2024, 5, 12), y:  18.0982 },
-			{ x: new Date(2024, 5, 20), y:  20.3164 },
-			{ x: new Date(2024, 5, 28), y: 20.1206 },
-			{ x: new Date(2024, 6, 1), y:  19.3301 },
-			{ x: new Date(2024, 6, 3), y:   20.4709 },
-			{ x: new Date(2024, 6, 8), y:  21.3724},
-			{ x: new Date(2024, 6, 11), y:  21.9130  },
-			{ x: new Date(2024, 6, 18), y: 20.9628 },
-			{ x: new Date(2024, 6, 24), y: 19.3483},
-			{ x: new Date(2024, 6, 30), y:  18.3641 },
-			{ x: new Date(2024, 6, 31), y:  20.1790  },
-			{ x: new Date(2024, 7, 1), y:  19.3437 },
-			{ x: new Date(2024, 7, 2), y:  17.2221 },
-			{ x: new Date(2024, 7, 5), y:  13.5562},
-			{ x: new Date(2024, 7, 6), y:  15.3616 }
-
-
-
-    ]
-	},
 	{
 		type: "line",
 		name: "VIX",
 		color: "#f50505",
-    axisYIndex: 1,
+    axisYIndex: 2,
 		axisYType: "secondary",
 		showInLegend: true,
     lineThickness: 5,
@@ -720,11 +435,9 @@ var chart1 = new CanvasJS.Chart("chartContainer1", {
 
 
 		]
-	}
-]
+	}]
 });
 chart.render();
-chart1.render();
 
 function toggleDataSeries(e) {
 	if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
@@ -736,7 +449,6 @@ function toggleDataSeries(e) {
 }
 
 }
-
 
 
 
